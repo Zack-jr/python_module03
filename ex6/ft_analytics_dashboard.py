b710877d03a2427396050f301d3b62d9d03ab1f1
Zack-jr <girardzack04@gmail,com>
2026-02-17 19:40:16 +0100

    ex6 remaining

---
+++ ex6/ft_analytics_dashboard.py
@@ -0,0 +1,22 @@
+class Player():
+    def __init__(self, player_name):
+        self.player_name = player_name
+
+
+def ft_analytics_dashboard():
+    print("=== Game Analytics Dashboard ===\n")
+    print("=== List Comprehension Example ===")
+
+
+if __name__ == '__main__':
+    ft_analytics_dashboard()
+
+
+
+
+
+
+# besoin d'un dict avec le nom de chaque joueur en cle et leur
+ # score en valeur
+
+ # list comprehension = appliquer une expression a chaque item d'une list/tuple/range
