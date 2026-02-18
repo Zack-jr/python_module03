9919a757b9d828c00dd960163a7ad57b92ac1905
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-02-18 13:31:47 +0100

    quick save

--- ex6/ft_analytics_dashboard.py
+++ ex6/ft_analytics_dashboard.py
@@ -1,11 +1,28 @@
 class Player():
-    def __init__(self, player_name):
+    def __init__(self, player_name, player_score, status):
         self.player_name = player_name
+        self.player_score = player_score
+        self.status = status
+
 
 
 def ft_analytics_dashboard():
     print("=== Game Analytics Dashboard ===\n")
     print("=== List Comprehension Example ===")
+
+    alice = Player("alice", 2700, "active")
+    bob = Player("bob", 2100, "inactive")
+    charlie = Player("charlie", 1200, "inactive")
+    diana = Player("diana", 2800, "active")
+
+    old_list = [alice, bob, charlie, diana]
+    high_scores = [x.player_name for x in old_list if x.player_score > 2000]
+    scores_doubled = [(x.player_score  * 2)for x in old_list]
+    active_players = [x.player_name for x in old_list if x.status == "active"]
+
+    print(f"High scorers (>2000): {high_scores}")
+    print(f"Scores doubled: {scores_doubled}")
+    print(f"Active players: {active_players}")
 
 
 if __name__ == '__main__':
@@ -20,3 +37,4 @@ if __name__ == '__main__':
  # score en valeur
 
  # list comprehension = appliquer une expression a chaque item d'une list/tuple/range
+ # ex : newlist = [x for x in fruits if "a" in x]
